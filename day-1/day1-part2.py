022b649597d456d786f3b3826fc52c3ecd527658
henrybarklam <72357080+henrybarklam@users.noreply.github.com>
2023-12-02 20:43:43 -0500

    Second one working

--- day-1/day1-part2.py
+++ day-1/day1-part2.py
@@ -1,5 +1,5 @@
 
-toRead = open('list.txt', 'r')
+toRead = open('listp2.txt', 'r')
 linesRead = toRead.readlines()
 
 class DecoderClass:
@@ -9,8 +9,12 @@ class DecoderClass:
         for line in lines:
             count+= self.decoder(line=line)
         return count
-
+    
     def decoder(self, line: str) -> int:
+
+        list_of_numbers = {"one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
+        starter_letters = list("otfsen")
+        ending_letters = list("eorxnt")
         found=False
         start, end, initial, final = 0, -1, 0, 0
 
@@ -18,20 +22,34 @@ class DecoderClass:
             if line[start].isnumeric():
                 initial = int(line[start])
                 found = True
-            else:
-                start+=1
+
+            elif line[start] in starter_letters:
+                checker = 3
+                while not found and start+checker < len(line) and checker <= 5:
+                    for number in list_of_numbers:
+                        if number in line[start:start+checker]:
+                            initial = list_of_numbers[number]
+                            found= True
+                    checker+=1
+            start+=1
         found = False
 
         while abs(end) <= len(line) and not found:
             if(line[end].isnumeric()):
                 final = int(line[end])
                 found = True
-            else:
-                end-=1
+            elif line[end] in ending_letters:
+                checker = 2
+                while not found and abs(end-checker) <= len(line) and checker <= 6:
+                    for number in list_of_numbers:
+                        if number in line[end-checker:]:
+                            final = list_of_numbers[number]
+                            found = True
+                    checker+=1
+            
+            end-=1
 
         return initial*10+final
         
             
 print(DecoderClass().decode(linesRead))
-
-
